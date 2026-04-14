14d3428f073f42e6634e497c387f711dacbcd472
Makar <Makarbelov2007@gmail.com>
2026-04-15 01:19:34 +0300

    Added new conditions for selecting a barcode type to the EncoderFactory

--- src/factories/encoder_factory.cpp
+++ src/factories/encoder_factory.cpp
@@ -4,5 +4,9 @@ IEncoder* EncoderFactory::Create(const std::string& type) {
 	if (type == "Aztec") {
 		return new AztecEncoder();
 	}
+	if (type == "QR") {
+		return new QREncoder();
+	if (type == "DataMatrix") {
+		return new DataMatrixEncoder();
 	throw std::invalid_argument("Unknown encoder type: " + type);
 }
