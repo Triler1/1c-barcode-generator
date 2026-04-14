2332629bf2d69af1efa9a712f531ea86c8cf2d35
Makar <Makarbelov2007@gmail.com>
2026-04-14 23:57:31 +0300

    I extracted the barcode size into a named constant, rechecked the aztek_encoder header file, and added comments

--- include/encoders/aztec_encoder.h
+++ include/encoders/aztec_encoder.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"IEncoder.h"
 #include <BitMatrix.h>
-
+// Энкодер для генерации штрихкодов формата Aztec
 class AztecEncoder : public IEncoder {
 public:
 	ZXing::BitMatrix Encode(const std::string& data) override;
--- src/encoders/aztec_encoder.cpp
+++ src/encoders/aztec_encoder.cpp
@@ -3,7 +3,8 @@
 #include <MultiFormatWriter.h>
 
 ZXing::BitMatrix AztecEncoder::Encode(const std::string& data) {
+    const int BarcodeSize = 200;
     auto writer = ZXing::MultiFormatWriter(ZXing::BarcodeFormat::Aztec);
-    auto matrix = writer.encode(data, 200, 200);
+    auto matrix = writer.encode(data, BarcodeSize, BarcodeSize);
     return matrix;
 }
