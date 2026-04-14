da77b6599fdc734892c81cf9319a334cf26412b6
muradnasirov63-droid <muradnasirov63@gmail.com>
2026-04-15 02:04:16 +0300

    add std::string overload to encoder API

--- include/encoders/datamatrix_encoder.h
+++ include/encoders/datamatrix_encoder.h
@@ -5,6 +5,7 @@
 class DataMatrixEncoder {
 public:
     static ZXing::BitMatrix encode(const std::wstring& text, int width, int height);
-
     static ZXing::BitMatrix encode(const std::wstring& text);
+
+    static ZXing::BitMatrix encode(const std::string& text);
 };
