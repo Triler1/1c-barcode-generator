55a4e2c88752779046e3dac646cfbd0a97a6821c
muradnasirov63-droid <muradnasirov63@gmail.com>
2026-04-15 01:45:04 +0300

    declare encode overload with default size

--- include/encoders/datamatrix_encoder.h
+++ include/encoders/datamatrix_encoder.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
-#include "BitMatrix.h" 
+#include "BitMatrix.h"
 
 class DataMatrixEncoder {
 public:
     static ZXing::BitMatrix encode(const std::wstring& text, int width, int height);
+
+    static ZXing::BitMatrix encode(const std::wstring& text);
 };
