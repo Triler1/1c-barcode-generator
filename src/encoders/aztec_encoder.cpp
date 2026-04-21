94fa2e443a927ec0af10c9fb29deced607ced4a2
Makar <Makarbelov2007@gmail.com>
2026-04-21 20:45:19 +0300

    Added an argument to the function

--- src/encoders/aztec_encoder.cpp
+++ src/encoders/aztec_encoder.cpp
@@ -1,18 +1,15 @@
-#include "AztecEncoder.h"
+#include "encoders/aztec_encoder.h"
 #include <BarcodeFormat.h>
 #include <MultiFormatWriter.h>
 #include <stdexcept>
 
-BarcodeMatrix AztecEncoder::Encode(const std::string& data) {
+BarcodeMatrix AztecEncoder::Encode(const std::string& data, const BarcodeOptions& options) const {
     if (data.empty()) {
         throw std::invalid_argument("Data must not be empty");
     }
-
     const int BarcodeSize = 200;
     auto writer = ZXing::MultiFormatWriter(ZXing::BarcodeFormat::Aztec);
     auto zxMatrix = writer.encode(data, BarcodeSize, BarcodeSize);
-
-
     BarcodeMatrix result(zxMatrix.width(), zxMatrix.height());
     for (int y = 0; y < zxMatrix.height(); y++) {
         for (int x = 0; x < zxMatrix.width(); x++) {
