a09c81e46a5fcb67e57c2160854c07de79da580f
Triler1 <q56875108@gmail.com>
2026-04-22 17:58:51 +0300

    update EncoderFactory to use std::unique_ptr

--- src/factories/encoder_factory.cpp
+++ src/factories/encoder_factory.cpp
@@ -1,18 +1,21 @@
 #include "factories/encoder_factory.h"
+
 #include "encoders/aztec_encoder.h"
 #include "encoders/qr_encoder.h"
 #include "encoders/datamatrix_encoder.h"
-#include <stdexcept>
 
-IEncoder* EncoderFactory::Create(const BarcodeType& type) {
+#include <stdexcept>
+#include <memory>
+
+std::unique_ptr<IEncoder> EncoderFactory::Create(BarcodeType type) {
     switch (type) {
-    case BarcodeType::Aztec:
-        return new AztecEncoder();
-    case BarcodeType::QR:
-        return new QREncoder();
-    case BarcodeType::DataMatrix:
-        return new DataMatrixEncoder();
-    default:
-        throw std::invalid_argument("Unknown encoder type");
+        case BarcodeType::Aztec:
+            return std::make_unique<AztecEncoder>();
+        case BarcodeType::QR:
+            return std::make_unique<QREncoder>();
+        case BarcodeType::DataMatrix:
+            return std::make_unique<DataMatrixEncoder>();
+        default:
+            throw std::invalid_argument("Unsupported barcode type");
     }
 }
