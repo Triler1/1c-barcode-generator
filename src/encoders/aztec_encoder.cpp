a72d2d1ca45398e9786bc6bbba6cbc234e9b4358
Makar <Makarbelov2007@gmail.com>
2026-05-14 13:43:23 +0300

    add new API ZXING writer

--- src/encoders/aztec_encoder.cpp
+++ src/encoders/aztec_encoder.cpp
@@ -1,20 +1,24 @@
 #include "encoders/aztec_encoder.h"
-#include <BarcodeFormat.h>
-#include <MultiFormatWriter.h>
-#include <BitMatrix.h>
+#include "CreateBarcode.h"
+#include "BitMatrix.h"
 #include <stdexcept>
 
 BarcodeMatrix AztecEncoder::Encode(const std::string& data, const BarcodeOptions& options) const {
     if (data.empty()) {
         throw std::invalid_argument("Data must not be empty");
     }
-    const int BarcodeSize = 200;
-    auto writer = ZXing::MultiFormatWriter(ZXing::BarcodeFormat::Aztec);
-    auto zxMatrix = writer.encode(data, BarcodeSize, BarcodeSize);
-    BarcodeMatrix result(zxMatrix.width(), zxMatrix.height());
-    for (int y = 0; y < zxMatrix.height(); y++) {
-        for (int x = 0; x < zxMatrix.width(); x++) {
-            result.Set(x, y, zxMatrix.get(x, y));
+
+    auto creatorOpts = ZXing::CreatorOptions(ZXing::BarcodeFormat::AztecCode);
+    auto barcode = ZXing::CreateBarcodeFromText(data, creatorOpts);
+
+    auto imageView = barcode.symbol();
+    int w = imageView.width();
+    int h = imageView.height();
+
+    BarcodeMatrix result(w, h);
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            result.Set(x, y, imageView.data()[y * imageView.rowStride() + x] == 0);
         }
     }
     return result;
